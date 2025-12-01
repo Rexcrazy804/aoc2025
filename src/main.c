daa6b7aec7660e7c5211358d0c23c746137127dd
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-01 11:54:04 +0400

    day 1 part 2 complete :D
    now we work on a good frame work for the coming days

--- src/main.c
+++ src/main.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int day1_p1(void);
-int rotate(int, int, char);
+int day1_p1(FILE *);
+void rotate(int *, int *, int, char);
 
 int main(int argc, char **argv) {
-  int answer = day1_p1();
+  FILE *test = fopen("./inputs/day1t.txt", "r");
+  int answer = day1_p1(test);
+  printf("test: %d\n", answer);
+  fclose(test);
 
-  printf("%d\n", answer);
+  if (answer != 6) {
+    printf("test failed :<\n");
+    return -1;
+  }
+
+  FILE *actual = fopen("./inputs/day1.txt", "r");
+  answer = day1_p1(actual);
+  printf("actual: %d\n", answer);
+  fclose(actual);
 
   return 0;
 }
 
-int day1_p1(void) {
-  FILE *file;
-  char direction;
-  char current;
-  int weight;
-  int dial;
-  int passwd;
-
-  file = fopen("./inputs/day1.txt", "r");
+int day1_p1(FILE *file) {
+  char direction, current;
+  int weight, dial, passwd, rotations;
 
   if (!file) {
     printf("failed to open file\n");
@@ -31,42 +36,39 @@ int day1_p1(void) {
   dial = 50;
   passwd = 0;
   direction = '0';
+  rotations = 0;
   while ((current = fgetc(file)) != EOF) {
     if (current == 'L' || current == 'R') {
-      if (direction != '0') {
-        dial = rotate(dial, weight, direction);
-        if (dial == 0) {
-          passwd += 1;
-        }
-      };
-
       direction = current;
       weight = 0;
     } else if (strchr("0123456789", current) != NULL) {
       weight = weight * 10 + atoi(&current);
+    } else if (current == '\n') {
+      rotate(&dial, &rotations, weight, direction);
+      passwd += (dial == 0) ? 1 : 0;
     }
   }
 
-  fclose(file);
-
-  return passwd;
+  // part one is just the passwd, part 2 we needa add the rotations as well
+  return passwd + rotations;
 }
 
-int rotate(int base, int weight, char direction) {
-  // printf("[PRE] dial:%d, weight: %d, direction: %C\n", base, weight,
-  // direction);
+void rotate(int *base, int *rotations, int weight, char direction) {
+  *rotations += weight / 100;
   weight = weight % 100;
 
   if (direction == 'L') {
-    base = base - weight;
+    if (*base > 0 && *base - weight < 0) {
+      *rotations += 1;
+    }
+    *base = *base - weight;
   } else {
-    base = base + weight;
+    if (*base < 100 && *base + weight > 100) {
+      *rotations += 1;
+    }
+    *base = *base + weight;
   }
 
-  base = base + 100;
-  base = base % 100;
-
-  // printf("[PST] dial:%d, weight: %d, direction: %C\n", base, weight,
-  // direction);
-  return base;
+  *base = *base + 100;
+  *base = *base % 100;
 }
