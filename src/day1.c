f209ba91fe8fce544274358bf6177a89591de1db
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-02 18:17:10 +0400

    standardize day1

--- src/day1.c
+++ src/day1.c
@@ -1,10 +1,26 @@
-// THIS FILE HAS NOT BEEN STANDARDIZED
-
-#include "day1.h"
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "day1.h"
+
 static void rotate(int *, int *, int, char);
+
+int day_driver(FILE *test, FILE *input) {
+  const int p1_ans = 3;
+  const int p2_ans = 6;
+
+  assert(p1_ans == day1_p1(test));
+  printf("P1: %d\n", day1_p1(input));
+
+  rewind(test);
+  rewind(input);
+
+  assert(p2_ans == day1_p2(test));
+  printf("P2: %d\n", day1_p2(input));
+
+  return 0;
+}
 
 int day1_p1(FILE *file) {
   char direction, current;
@@ -31,7 +47,34 @@ int day1_p1(FILE *file) {
     }
   }
 
-  // part one is just the passwd, part 2 we needa add the rotations as well
+  return passwd;
+}
+
+int day1_p2(FILE *file) {
+  char direction, current;
+  int weight, dial, passwd, rotations;
+
+  if (!file) {
+    printf("failed to open file\n");
+    return 0;
+  };
+
+  dial = 50;
+  passwd = 0;
+  direction = '0';
+  rotations = 0;
+  while ((current = (char)fgetc(file)) != EOF) {
+    if (current == 'L' || current == 'R') {
+      direction = current;
+      weight = 0;
+    } else if (strchr("0123456789", current) != NULL) {
+      weight = weight * 10 + atoi(&current);
+    } else if (current == '\n') {
+      rotate(&dial, &rotations, weight, direction);
+      passwd += (dial == 0) ? 1 : 0;
+    }
+  }
+
   return passwd + rotations;
 }
 
--- src/day1.h
+++ src/day1.h
@@ -4,6 +4,8 @@
 #ifndef __DAY1_H__
 #define __DAY1_H__
 
+int day_driver(FILE *, FILE *);
 int day1_p1(FILE *);
+int day1_p2(FILE *);
 
 #endif
